222e376b290b2546057aa58ce11809b558d2c607
Sabir Mondal <mondal.sabir.com97@gmail.com>
2021-01-16 13:47:39 +0530

    UI ready

--- app/src/main/java/com/quapps/dinder/MainActivity.java
+++ app/src/main/java/com/quapps/dinder/MainActivity.java
@@ -1,14 +1,40 @@
 package com.quapps.dinder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView dogImageView;
+    ImageButton likeImageButton;
+    ImageButton dislikeImageButton;
+    RecyclerView recyclerView;
+    List<String> imageList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dogImageView=findViewById(R.id.dogImageView);
+        likeImageButton=findViewById(R.id.likeImageButton);
+        dislikeImageButton=findViewById(R.id.dislikeImageButton);
+        findViewById(R.id.recyclerView);
+        likeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Like */
+            }
+        });
+        dislikeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Dislike*/
+            }
+        });
     }
 }
